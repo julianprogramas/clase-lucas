0a7c493800d926e4407776e7271120f2fe3385ac
julian calvo <juliderc@gmail.com>
2022-09-12 20:27:44 -0300

    cree carpetas

---
+++ js/scrip-port.js
@@ -0,0 +1,19 @@
+let nombre = prompt('igrese su nombre');
+alert(nombre)
+let edad = prompt('igrese su edad');
+alert(edad)
+let anio = prompt('igrese su año');
+alert('sos del año '+ anio)
+
+
+
+let numero = 10;
+numero  += 2
+ numero  -= 2
+numero  *= 2
+numero  /= 2
+numero  %= 2
+numero  **= 2
+
+/*se pueden usar cualquiera de estos valores*/ 
+document.write(numero)
---
+++ js/scrip.js
@@ -0,0 +1,41 @@
+/*  text  */
+/*  let name = 'julian';
+let last_name = 'calvo';
+let name_complete;  */
+
+/* number */
+/* let weight  = 70;
+let year = 25; 
+ */
+/* const */
+ 
+/* const FAN = 'racing'
+
+const IVA = 21; */
+
+/* Var Numericas */
+
+
+/* let numberA = 4 ;
+let numberB= 5 ;
+const PI = 9 ;
+
+
+let suma = 5 + 4;
+let resta = numberA-numberB;
+let produt = numberA*numberB;
+let module = numberB % numberA;
+
+
+let word1= 'hola';
+let word2= 'juli';
+let remate = 'aguante_racing';
+let number = 25;
+
+
+ */
+
+let edadActual = parseInt(prompt('ingresa tu edad actual'));
+let edadEn10 = edadActual + 10;
+console.log(edadEn10);
+alert('En 10 años tendras '+ edadEn10 + ' años 50');
